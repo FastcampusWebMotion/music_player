797276212d05af36858bc4379a2002d079172c40
tubi55 <tubi55@nate.com>
2022-01-08 20:25:28 +0900

    좌우버튼 클릭시 프레임 회전

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 const frame = document.querySelector('section');
 const list = frame.querySelectorAll('article');
+const prev = document.querySelector('.btnPrev');
+const next = document.querySelector('.btnNext');
 const len = list.length;
 const deg = 360/len;
+let num = 0;
 
 for(let i=0; i<len; i++){
     list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
 }
+
+prev.addEventListener('click', e=>{
+    frame.style.transform = `rotate(${deg* ++num}deg)`
+})
+
+next.addEventListener('click', e=>{
+    frame.style.transform = `rotate(${deg* --num}deg)`
+})
