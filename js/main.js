7e915356fd1f8e0718fd7252048acdecbc25ba6f
tubi55 <tubi55@nate.com>
2022-01-08 22:35:02 +0900

    패널 컨텐츠 추가

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ const frame = document.querySelector('section');
 const list = frame.querySelectorAll('article');
 const prev = document.querySelector('.btnPrev');
 const next = document.querySelector('.btnNext');
+const names = ['Blizzards', 'Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
 const len = list.length;
 const deg = 360/len;
 let num = 0;
 let active = 0;
 
-for(let i=0; i<len; i++){
-    list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
-}
+names.forEach((name, index)=>{
+    const pic = list[index].querySelector('.pic');
+    const h2 = list[index].querySelector('.txt h2');
+    list[index].style.transform = `rotate(${deg*index}deg) translateY(-100vh)`;
+    pic.style.backgroundImage = `url(img/${name}.jpg)`;
+    h2.innerText = name;
+})
+
+
 
 prev.addEventListener('click', e=>{
     frame.style.transform = `rotate(${deg* ++num}deg)`;
