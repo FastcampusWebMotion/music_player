c68018134c7a8aa16be6b6962308e3d076b70325
tubi55 <tubi55@nate.com>
2022-01-08 21:27:32 +0900

    좌우버튼 클릭시 가운데 패널 활성화

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ const next = document.querySelector('.btnNext');
 const len = list.length;
 const deg = 360/len;
 let num = 0;
+let active = 0;
 
 for(let i=0; i<len; i++){
     list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
 }
 
 prev.addEventListener('click', e=>{
-    frame.style.transform = `rotate(${deg* ++num}deg)`
+    frame.style.transform = `rotate(${deg* ++num}deg)`;
+
+    (active===0) ? active = len-1 : active--;   
+    for(let el of list) el.classList.remove('on');
+    list[active].classList.add('on');
 })
 
 next.addEventListener('click', e=>{
-    frame.style.transform = `rotate(${deg* --num}deg)`
+    frame.style.transform = `rotate(${deg* --num}deg)`;
+
+    (active === len-1) ? active = 0 : active++;  
+    for(let el of list) el.classList.remove('on');
+    list[active].classList.add('on');
 })
